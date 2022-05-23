fix bug in duration calculation where responses that never went below threshold in time window excepted to zero duration (e.g. nkx)

tools/measurements.py
@@ -30,7 +30,11 @@ def responseDuration(data,thresh=.3):
     x = np.median(data,axis=0)
     try:
         return np.where(x<=thresh)[0][0]/120
-    except: return 1/120 #return small value so don't get a divide by zero in comparison tests
+    except: 
+        if x.mean()>thresh: #if didn't go below threshold in sample window
+            return x.size/120
+        else:
+            return 1/120 #return small value so don't get a divide by zero in comparison tests
 
 def peakResponse(data):
     '''peak response of population trace'''
@@ -38,7 +42,7 @@ def peakResponse(data):
 
 def totalResponse_pop(data):
     '''total response of population trace'''
-    return np.mean(np.var((data>.5).astype(float),axis=0))#np.mean(np.median(data,axis=0))
+    return np.mean(np.median(data[:,:10*120],axis=0))#np.mean(np.var((data>.5).astype(float),axis=0))#
 
 def totalResponse(data):
     '''total response of individual trace'''

tools/results.py
@@ -136,11 +136,11 @@ def rnai_response_layered(interest_list,exclude,n_boot=1e3,statistic=np.median,d
 #             y,rng = bootstrap(yp_ref[:,loc:loc+10*120],n_boot=n_boot,statistic=M)
             bott = 0
             if measure_compare in DIFFERENCE:
-                y,rng,significant = bootstrap_diff(yp_ref[:,loc:loc+10*120],yp_ref[:,loc:loc+10*120]
+                y,rng,significant = bootstrap_diff(yp_ref[:,loc:loc+15*120],yp_ref[:,loc:loc+15*120]
                                            ,n_boot=n_boot_meas,measurement=M,conf_interval=conf_interval)
             elif measure_compare in RELATIVE:
                 bott = 1
-                y,rng,significant = bootstrap_relative(yp_ref[:,loc:loc+10*120],yp_ref[:,loc:loc+10*120]
+                y,rng,significant = bootstrap_relative(yp_ref[:,loc:loc+15*120],yp_ref[:,loc:loc+15*120]
                                            ,n_boot=n_boot_meas,measurement=M,conf_interval=conf_interval)
             else:
                 y,rng = bootstrap(yp_ref[:,loc:loc+10*120],n_boot=n_boot_meas,statistic=M,conf_interval=conf_interval)
@@ -156,7 +156,7 @@ def rnai_response_layered(interest_list,exclude,n_boot=1e3,statistic=np.median,d
         #calculate reference individual statistics
         for n_m2, M in enumerate(ind_measure):
             loc=np.argmin(xp**2)
-            value = M(yp_ref[:,loc:loc+10*120])
+            value = M(yp_ref[:,loc:loc+15*120])
             if measure_compare in DIFFERENCE:
                 y,rng,significant = bootstrap_diff(value,value,n_boot=n_boot_meas,measurement=None,conf_interval=conf_interval)
             elif measure_compare in RELATIVE:
@@ -222,11 +222,11 @@ def rnai_response_layered(interest_list,exclude,n_boot=1e3,statistic=np.median,d
                     loc=np.argmin(xp**2)
                     bott = 0
                     if measure_compare in DIFFERENCE:
-                        y,rng,significant = bootstrap_diff(yp[:,loc:loc+10*120],Y_REF[num][:,loc:loc+10*120]
+                        y,rng,significant = bootstrap_diff(yp[:,loc:loc+15*120],Y_REF[num][:,loc:loc+15*120]
                                                    ,n_boot=n_boot_meas,measurement=M,conf_interval=conf_interval)
                     elif measure_compare in RELATIVE:
                         bott=1
-                        y,rng,significant = bootstrap_relative(yp[:,loc:loc+10*120],Y_REF[num][:,loc:loc+10*120]
+                        y,rng,significant = bootstrap_relative(yp[:,loc:loc+15*120],Y_REF[num][:,loc:loc+15*120]
                                                    ,n_boot=n_boot_meas,measurement=M,conf_interval=conf_interval)
                     else:
                         y,rng = bootstrap(yp[:,loc:loc+10*120],n_boot=n_boot_meas,statistic=M,conf_interval=conf_interval)
@@ -241,8 +241,8 @@ def rnai_response_layered(interest_list,exclude,n_boot=1e3,statistic=np.median,d
                 #calculate individual based
                 for n_m2, M in enumerate(ind_measure):
                     loc=np.argmin(xp**2)
-                    value = M(yp[:,loc:loc+10*120])
-                    value_ref = M(Y_REF[num][:,loc:loc+10*120])
+                    value = M(yp[:,loc:loc+15*120])
+                    value_ref = M(Y_REF[num][:,loc:loc+15*120])
                     if measure_compare in DIFFERENCE:
                         y,rng,significant = bootstrap_diff(value,value_ref,n_boot=n_boot_meas,measurement=None,conf_interval=conf_interval)
                     elif measure_compare in RELATIVE: