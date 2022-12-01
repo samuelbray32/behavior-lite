a03254db3d0d85289d83690a1948dc081f5b2319
samuelbray32 <sambray@stanford.edu>
2022-12-01 12:00:49 -0800

    white noise analysis first commit

---
+++ tools/results_noise.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from .bootstrapTest import bootstrap_traces, bootstrapTest, bootstrap, timeDependentDifference
+from .bootstrapTest import bootstrap_diff, bootstrap_relative
+
+def data_of_interest(names,interest=[],exclude=[]):
+    to_plot = []
+    for dat in names:
+        if dat in to_plot: continue
+        for i in interest:
+            if i in dat:
+                keep = True
+                for ex in exclude:
+                    if ex in dat: keep = False
+                #check double/triple knockdown
+                if dat.count('+')>i.count('+'):
+                    print(dat)
+                    keep=False
+                if keep: to_plot.append(dat)
+    return to_plot
+
+
+def autocovariance(interest=[],exclude=[],tau=np.arange(1,120*10,10),
+                    sample_range=(15,30),
+                    n_boot=1e3,statistic=np.median,
+                    measure_compare=None,ind_measure=[],
+                    pop_measure=[],baseline=128,
+                    conf_interval=95, stat_testing=True,):
+    '''
+    Haphazard version to get single plot for my defense. Promise to make it better -SB 101922
+    '''
+
+    to_plot = ['101422WT_30m_128Mean_64Amp_100msRefresh']
+    to_plot = ['102122WT_60m_128Mean_64Amp_100msRefresh']
+    #loop the sin periods
+    fig,ax = plt.subplots(ncols=1,figsize=(6,6))
+    from tools.measurements import cross_correlate_auto
+    name = 'data/LDS_response_Noise.pickle'
+    with open(name,'rb') as f:
+        result = pickle.load(f)
+    xp = result['tau']
+    yp = np.concatenate([result[dat] for dat in to_plot])
+    print(to_plot)
+
+    ind_t = np.where((xp>sample_range[0])&(xp<sample_range[1]))[0]
+    C = np.array([cross_correlate_auto(yy,tau) for yy in yp[:,ind_t]])
+    def med(x):
+        return np.mean(x,axis=0)
+    y,rng,dist=bootstrap(C,statistic=med,conf_interval=conf_interval,n_boot=n_boot,
+        return_samples=True,)
+    # y,rng,dist=bootstrap(yp[:,ind_t],statistic=cross_correlate_auto,conf_interval=conf_interval,n_boot=n_boot,
+    #     return_samples=True,tau=tau)
+    c='cornflowerblue'
+    ax.plot(tau/120,y,c=c)
+    ax.fill_between(tau/120,*rng,facecolor=c,alpha=.2)
+    ax.plot(tau/120,tau*0,c='grey',zorder=-10)
+    ax.set_xlabel('$\\tau$')
+    ax.set_ylabel('Autocovariance')
+    return fig,ax
+
+def autocovariance_step(interest_list,exclude=[],powers =[64,],
+                    tau=np.arange(1,120*10,10),
+                    sample_range=(15,30),
+                    n_boot=1e3,statistic=np.median,
+                    measure_compare=None,ind_measure=[],
+                    pop_measure=[],conf_interval=95, stat_testing=True,):
+    '''
+    Haphazard version to get single plot for my defense. Promise to make it better -SB 101922
+    #adapted to look at changes in a step function
+    '''
+    #loop the sin periods
+    fig,ax = plt.subplots(ncols=1,figsize=(6,6))
+    from tools.measurements import cross_correlate_auto
+    name = 'data/LDS_response_LONG.pickle'
+    with open(name,'rb') as f:
+        result = pickle.load(f)
+
+    for i,interest in enumerate(interest_list):
+        print(interest)
+        c=plt.cm.Set1(i/9)
+        for num,power in enumerate(powers):
+            exclude_this = exclude.copy()
+            if not '+' in interest:
+                exclude_this.append('+')
+            to_plot = data_of_interest(result.keys(),[f'{interest}_30m2h{power}bp'],exclude_this)
+            print(f'{interest}_30m2h{power}bp',to_plot)
+            if len(to_plot)==0: continue
+            yp=[]
+            for dat in to_plot:
+                yp.extend(result[dat])
+            yp = np.array(yp)
+            print(to_plot)
+            xp = result['tau']
+            ind_t = np.where((xp>sample_range[0])&(xp<sample_range[1]))[0]
+            C = np.array([cross_correlate_auto(yy,tau) for yy in yp[:,ind_t]])
+            def med(x):
+                return np.mean(x,axis=0)
+            y,rng,dist=bootstrap(C,statistic=med,conf_interval=conf_interval,n_boot=n_boot,
+                return_samples=True,)
+            # y,rng,dist=bootstrap(yp[:,ind_t],statistic=cross_correlate_auto,conf_interval=conf_interval,n_boot=n_boot,
+            #     return_samples=True,tau=tau)
+            # c='cornflowerblue'
+            ax.plot(tau/120,y,c=c,label=interest)
+            ax.fill_between(tau/120,*rng,facecolor=c,alpha=.2)
+            ax.plot(tau/120,tau*0,c='grey',zorder=-10)
+    ax.set_xlabel('$\\tau$')
+    ax.set_ylabel('Autocovariance')
+    ax.legend()
+    return fig,ax
